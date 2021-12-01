2527ca6ff7dac6fde110060b5c9a22891e352071
gatraraditya <gatra.raditya@fit.id>
2021-12-01 13:50:48 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  tags = ["tfm", "web"]
+
+  # allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      # image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "tfm-allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20", "66.96.231.26"]
+}
